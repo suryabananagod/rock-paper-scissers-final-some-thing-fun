bcab694e603b7ff909070a62ccfa9ea00b201a5f
suryabananagod <suryaporwal757@gmail.com>
2026-09-17 19:27:00 +0530

    Note:
    not working currently work in progress will be compleat in 3-4 day

---
+++ rock_paper_scisers.py
@@ -0,0 +1,23 @@
+import random
+import pygame
+
+class Button():
+    def __init__(self, x, y, pos, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.pos = pos
+
+    def clicked(self, pos):
+        self.pos = pygame.mouse.get_pos()
+        if self.pos[0] > self.x and self.pos[0] < self.x + self.width:
+            if self.pos[1] > self.y and self.pos[1] < self.y + self.height:
+                return True
+        return False
+class RpsGame():
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((960, 640))
+        pygame.display.set_caption("RPS Smasher")
